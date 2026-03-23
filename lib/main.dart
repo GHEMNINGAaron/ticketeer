e1c0d17331067e4e1accff0a9b49c58a4ceb2f2b
GHEMNINGAaron <aaronkom345kata@gmail.com>
2026-03-23 17:54:52 +0100

    Update app title from 'Ticketeer' to 'Ticketeers'

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Ticketeer',
+      title: 'Ticketeers',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: 'Roboto',
